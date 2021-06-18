3373cddea5e34a358dea4719b8d5115f819533ea
Ryan B. Harvey <ryan.b.harvey@ted.com>
2021-06-18 17:58:48 -0500

    Chapter 1 complete: guess a number game with play again and score tracking

---
+++ lib/pento_web/live/wrong_live.ex
@@ -0,0 +1,103 @@
+defmodule PentoWeb.WrongLive do
+  use PentoWeb, :live_view
+
+  def mount(_params, _session, socket) do
+    if connected?(socket) do
+      # :timer.send_interval(1, :tick)
+      :timer.send_interval(1000, :tick)
+    end
+
+    {
+      :ok,
+      assign(
+        socket,
+        right_answer: Enum.random(1..10),
+        score: 0,
+        message: "Guess a number.",
+        display_time: time(),
+        won_game: nil
+      )
+    }
+  end
+
+  def render(assigns) do
+    ~L"""
+    <h1>Your score: <%= @score %></h1>
+    <p>It's <%= @display_time %></p>
+    <h2>
+      <%= @message %>
+      <%= render_button(assigns) %>
+    </h2>
+    <h2>
+      <%= for n <- 1..10 do %>
+      <a href="#" phx-click="guess", phx-value-number="<%= n %>"><%= n %></a>
+      <% end %>
+    </h2>
+    """
+  end
+
+  def handle_params(%{ "reset" => "true", "score" => score }=_params, _session, socket) do
+    {
+      :noreply,
+      assign(
+        socket,
+        right_answer: Enum.random(1..10),
+        score: score |> Integer.parse |> elem(0),
+        message: "Guess a number.",
+        display_time: time(),
+        won_game: nil
+      )
+    }
+  end
+  def handle_params(_params, _session, socket), do: {:noreply, socket}
+
+  def handle_info(:tick, socket) do
+    {:noreply, assign(socket, display_time: time())}
+  end
+
+  def handle_event("guess", %{ "number" => guess }=_data, socket) do
+    guess_value = guess |> Integer.parse |> elem(0)
+    won_game = guess_value == socket.assigns.right_answer
+    message = response_message(guess_value, won_game)
+    score = updated_score(socket.assigns.score, won_game)
+
+    {
+      :noreply,
+      assign(
+        socket,
+        message: message,
+        score: score,
+        display_time: time(),
+        won_game: won_game
+      )
+    }
+  end
+
+  def updated_score(score, true=_won_game), do: score + 10
+  def updated_score(score, _won_game), do: score - 1
+
+  def response_message(guess, true=_won_game), do: "Your guess: #{guess}. Correct! You win!"
+  def response_message(guess, false=_won_game), do: "Your guess: #{guess}. Wrong. Guess again."
+  def response_message(_guess, nil=_won_game), do: "Guess a number."
+
+  def render_button(assigns) do
+    ~L"""
+    <%= if @won_game == true do %>
+    <p>
+      <%= live_patch "Play again?",
+            to: Routes.live_path(
+              @socket,
+              PentoWeb.WrongLive,
+              reset: true,
+              score: @score
+            )
+      %>
+    </p>
+    <% end %>
+    """
+  end
+
+  def time() do
+    DateTime.utc_now |> to_string
+  end
+end
